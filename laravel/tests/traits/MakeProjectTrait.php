169b9512ee7dc4905f93ba6c2911665bd5074bbc
swatich123 <swati22chaudhary@gmail.com>
2016-04-19 15:31:11 +0530

    commit

---
+++ laravel/tests/traits/MakeProjectTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+use Faker\Factory as Faker;
+use App\Models\Project;
+use App\Repositories\ProjectRepository;
+
+trait MakeProjectTrait
+{
+    /**
+     * Create fake instance of Project and save it in database
+     *
+     * @param array $projectFields
+     * @return Project
+     */
+    public function makeProject($projectFields = [])
+    {
+        /** @var ProjectRepository $projectRepo */
+        $projectRepo = App::make(ProjectRepository::class);
+        $theme = $this->fakeProjectData($projectFields);
+        return $projectRepo->create($theme);
+    }
+
+    /**
+     * Get fake instance of Project
+     *
+     * @param array $projectFields
+     * @return Project
+     */
+    public function fakeProject($projectFields = [])
+    {
+        return new Project($this->fakeProjectData($projectFields));
+    }
+
+    /**
+     * Get fake data of Project
+     *
+     * @param array $postFields
+     * @return array
+     */
+    public function fakeProjectData($projectFields = [])
+    {
+        $fake = Faker::create();
+
+        return array_merge([
+            'created_at' => $fake->word,
+            'updated_at' => $fake->word
+        ], $projectFields);
+    }
+}
